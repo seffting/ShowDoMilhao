f7a1a92304e002d39b21c9417ff73837bca7a5f4
Sofia Effting <sofia.effting@philips.com>
2024-06-03 13:30:41 -0300

    sofia_feat_perguntas_main

---
+++ data/perguntas.js
@@ -0,0 +1,683 @@
+let perguntas = [
+  {
+    pergunta: "Quem salva a Bela Adormecida no clássico conto de fadas?",
+    escolha1: "O caçador",
+    escolha2: "O Príncipe Encantado",
+    escolha3: "A sua madrinha",
+    escolha4: " O anão",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual é o coletivo de cães?",
+    escolha1: "Matilha",
+    escolha2: "Rebanho",
+    escolha3: "Cardume",
+    escolha4: " Manada",
+    resposta: 1
+  },
+  {
+    pergunta: "O cajú é uma fruta encontrada na região: ",
+    escolha1: "Nordeste",
+    escolha2: "Norte",
+    escolha3: "Sudeste",
+    escolha4: " Centro-Oeste",
+    resposta: 1
+  },
+  {
+    pergunta: "Em que continente ficam a Costa do Marfim e Camarões?",
+    escolha1: "Europa",
+    escolha2: "África",
+    escolha3: "América",
+    escolha4: " Ásia",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual casal foi expluso do Paraíso?",
+    escolha1: "Sansão e Dalila",
+    escolha2: "José e Maria",
+    escolha3: "Sara e Abraão",
+    escolha4: " Adão e Eva",
+    resposta: 4
+  },
+  {
+    pergunta: "Segundo os contos, qual animal ao ser beijado se transforma em príncipe?",
+    escolha1: "Cavalo",
+    escolha2: "Cão",
+    escolha3: "Gato",
+    escolha4: " Sapo",
+    resposta: 4
+  },
+  {
+    pergunta: "Quem é o primeiro substituto do presidente?",
+    escolha1: "Vereador",
+    escolha2: "Vice-presidente",
+    escolha3: "Senador",
+    escolha4: " Deputado",
+    resposta: 2
+  },
+  {
+    pergunta: "Que animal é o Pateta?",
+    escolha1: "Cavalo",
+    escolha2: "Burro",
+    escolha3: "Cachorro",
+    escolha4: " Raposa",
+    resposta: 3
+  },
+  {
+    pergunta: "Qual o número mínimo que um mês pode ter?",
+    escolha1: "28",
+    escolha2: "29",
+    escolha3: "30 ",
+    escolha4: " 31",
+    resposta: 1
+  },
+  {
+    pergunta: "Que nome se da à mistura de água com sal?",
+    escolha1: "Salmora",
+    escolha2: "Gangorra",
+    escolha3: "Melado",
+    escolha4: " Bolacha",
+    resposta: 1
+  },
+  {
+    pergunta: "De onde as abelhas extraem mel?",
+    escolha1: "Das rochas",
+    escolha2: "Das flores",
+    escolha3: "Da água",
+    escolha4: " Da terra",
+    resposta: 2
+  },
+  {
+    pergunta: "Como se chama o ratinho criado por Walt Disney?",
+    escolha1: "Jerry",
+    escolha2: "Pernalonga",
+    escolha3: "Frajola",
+    escolha4: " Mickey Mouse",
+    resposta: 4
+  },
+  {
+    pergunta: "A acústica é uma ciência que estuda...",
+    escolha1: "Cometas",
+    escolha2: "Lua",
+    escolha3: "Sol",
+    escolha4: " Som",
+    resposta: 4
+  },
+  {
+    pergunta: "Qual parte dos patins usados na patinação no gelo os faz deslizar?",
+    escolha1: "Bota",
+    escolha2: "Lâmina",
+    escolha3: "Travão",
+    escolha4: " Rodas",
+    resposta: 2
+  },
+  {
+    pergunta: "Geralmente qual a cor dos olhos de um vampiro?",
+    escolha1: "Castanhos",
+    escolha2: "Amarelos",
+    escolha3: "Pretos",
+    escolha4: " Vermelhos",
+    resposta: 4
+  },
+  {
+    pergunta: "Qual foi o último estado criado no Brasil?",
+    escolha1: "Tocantins",
+    escolha2: "Acre",
+    escolha3: "Rondônia",
+    escolha4: " Mato Grosso do Sul",
+    resposta: 1
+  },
+  {
+    pergunta: "Em que país nasceu Carmem Miranda?",
+    escolha1: "Brasil",
+    escolha2: "Espanha",
+    escolha3: "Portugal",
+    escolha4: " Argentina",
+    resposta: 3
+  },
+  {
+    pergunta: "Quem escreveu o livro 'O Guarani'?",
+    escolha1: "Carlos Gomes",
+    escolha2: "Olavo Billac",
+    escolha3: "José de Alencar",
+    escolha4: " Machado de Assis",
+    resposta: 3
+  },
+  {
+    pergunta: "Quem foi o amor de Isolda na Obra Medieval?",
+    escolha1: "Romeu",
+    escolha2: "Tristão",
+    escolha3: "Rigoleto",
+    escolha4: " Hamlet",
+    resposta: 2
+  },
+  {
+    pergunta: "Como eram chamados os reis do antigo Egito?",
+    escolha1: "Generais",
+    escolha2: "Faraós",
+    escolha3: "Sheiks",
+    escolha4: " Presidentes",
+    resposta: 2
+  },
+  {
+    pergunta: "Quem é o primo doido do Pato Donald?",
+    escolha1: "Patinhas",
+    escolha2: "Zé Carioca",
+    escolha3: "Gastão",
+    escolha4: " Peninha",
+    resposta: 4
+  },
+  {
+    pergunta: "Qual o desenho animado que tem um pai de família chamado Homer?",
+    escolha1: "Família Adams",
+    escolha2: "Os Finstones",
+    escolha3: "Denis o Pimentinha",
+    escolha4: " Os Simpsons",
+    resposta: 4
+  },
+  {
+    pergunta: "Qual o nome do escritor de 'Gabriela Cravo e Canela'?",
+    escolha1: "Graciliano Ramos",
+    escolha2: "Érico Veríssimo",
+    escolha3: "Jorge Amado",
+    escolha4: " Machado de Assis",
+    resposta: 3
+  },
+  {
+    pergunta: "Por que foi dita a frase: 'Abre-te Sésamo'?",
+    escolha1: "Hércules",
+    escolha2: "D. Pedro I",
+    escolha3: "Dunga",
+    escolha4: " Ali Babá",
+    resposta: 4
+  },
+  {
+    pergunta: "Os nomes Batman e Rain Man referem-se a quê?",
+    escolha1: "Artistas de rock",
+    escolha2: "Filmes de cinema",
+    escolha3: "Artistas de teatro",
+    escolha4: " Marcas de cigarro",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual é o animal que puxa o trenó do Papai Noel?",
+    escolha1: "Rena",
+    escolha2: "Touro",
+    escolha3: "Jumento",
+    escolha4: " Cavalo",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem reinou durante o Segundo Império Francês?",
+    escolha1: "Napoleão Primeiro",
+    escolha2: "Carlos VII",
+    escolha3: "Maria Antonieta",
+    escolha4: "Napoleão III",
+    resposta: 4
+  },
+  {
+    pergunta: "Em que estado brasileiro ocorreu a Guerra dos Canudos?",
+    escolha1: "Sergipe",
+    escolha2: "Minas Gerais",
+    escolha3: "Bahia",
+    escolha4: "São Paulo",
+    resposta: 3
+  },
+  {
+    pergunta: "Que jogador da seleção foi acusado de 'amarelar' na final da copa de 98?",
+    escolha1: "Tafarel",
+    escolha2: "Romario",
+    escolha3: "Ronaldinho",
+    escolha4: "Rivaldo",
+    resposta: 3
+  },
+  {
+    pergunta: "Qual a árvore cujo ramo simboliza a paz?",
+    escolha1: "Seringueira",
+    escolha2: "Oliveira",
+    escolha3: "Carvalho",
+    escolha4: "Ipê",
+    resposta: 2
+  },
+  {
+    pergunta: "O satélite artificial Sputinik foi lançado pela União Soviética em:",
+    escolha1: "1950",
+    escolha2: "1955",
+    escolha3: "1957",
+    escolha4: "1960",
+    resposta: 3
+  },
+  {
+    pergunta: "Qual a capital do Irã?",
+    escolha1: "Iraque",
+    escolha2: "Croácia",
+    escolha3: "Teerã",
+    escolha4: "Tel Aviv",
+    resposta: 3
+  },
+  {
+    pergunta: "Que pintor brasileiro pintava a beleza da mulata e a sensualidade feminina?",
+    escolha1: "Di Cavalcanti",
+    escolha2: "Cândido Portinari",
+    escolha3: "Volpi",
+    escolha4: "Tarsila do Amaral",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual a nacionalidade do primeiro homem a pisar na Lua?",
+    escolha1: "Japonesa",
+    escolha2: "Russa",
+    escolha3: "Chinesa",
+    escolha4: " Norte-americana",
+    resposta: 4
+  },
+  {
+    pergunta: "Qual é o único animal mamífero que pode voar?",
+    escolha1: "Urubu",
+    escolha2: "Morcego",
+    escolha3: "Pato",
+    escolha4: " Andorinha",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual destes itens não é encontradp em uma bicicleta comum?",
+    escolha1: "Guidão",
+    escolha2: "Air Bag",
+    escolha3: "Corrente",
+    escolha4: " Selim",
+    resposta: 2
+  },
+  {
+    pergunta: "Em que dia nasceu Bill Gates?",
+    escolha1: "28 de setembro de 1957",
+    escolha2: "28 de outubro de 1955",
+    escolha3: "28 de fevereiro de 1960",
+    escolha4: "30 de outubro de 1950",
+    resposta: 2
+  },
+  {
+    pergunta: "Em que ano a Microsoft foi fundada?",
+    escolha1: "1973",
+    escolha2: "1974",
+    escolha3: "1975",
+    escolha4: "1976",
+    resposta: 3
+  },
+  {
+    pergunta: "Em que dia nasceu Mark Zuckerberg?",
+    escolha1: "14 de maio de 1984",
+    escolha2: "14 de abril de 1985",
+    escolha3: "14 de setembro de 1986",
+    escolha4: "14 de maio de 1980",
+    resposta: 1
+  },
+  {
+    pergunta: "Em que ano foi fundada a Riot Games?",
+    escolha1: "2003",
+    escolha2: "2009",
+    escolha3: "2005",
+    escolha4: "2006",
+    resposta: 4
+  },
+  {
+    pergunta: "Quais são as cores que se encontram na bvandeira do Japão?",
+    escolha1: "Vermelha e Amarela",
+    escolha2: "Azul e Branca",
+    escolha3: "Branca e Preta",
+    escolha4: " Vermelha e Branca",
+    resposta: 4
+  },
+  {
+    pergunta: "Qual o nome da vilã do filme 'Os 101 Dálmatas'?",
+    escolha1: "Cruella de Vil",
+    escolha2: "Drissela",
+    escolha3: "Eleanor",
+    escolha4: " Anastacia",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem protagonizou o filme 'Cisne Negro' e ganhou o Óscar de melhor atriz em 2011??",
+    escolha1: "Mila Kunis",
+    escolha2: "Jennifer Lawrence",
+    escolha3: "Natalie Portman",
+    escolha4: " Anne Hathaway",
+    resposta: 3
+  },
+  {
+    pergunta: "Nos contos medievais, quem era o líder dos cavaleiros da távola redonda?",
+    escolha1: "Rei Arthur",
+    escolha2: "Robin Hood",
+    escolha3: "Zorro",
+    escolha4: " D' Artagnan",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual dos seguintes não é um país sul-americano?",
+    escolha1: "Brasil",
+    escolha2: "Nova Zelândia",
+    escolha3: "Colômbia",
+    escolha4: " Bolívia",
+    resposta: 2
+  },
+  {
+    pergunta: "Quando um avião ultrapassa a velocidade do som, pode ser chamado de...",
+    escolha1: "Avião bala",
+    escolha2: "Supersônico",
+    escolha3: "Sônico",
+    escolha4: " Ultrasônico",
+    resposta: 2
+  },
+  {
+    pergunta: "Quem criou o personagem de banda desenhada Mafalda?",
+    escolha1: "Milo Manara",
+    escolha2: "Maurício de Sousa",
+    escolha3: "Quino",
+    escolha4: " Bill Watterson",
+    resposta: 3
+  },
+  {
+    pergunta: "Como é chamada a sede do Governo Argentino?",
+    escolha1: "Palácio Argentino",
+    escolha2: "Casa Rosada",
+    escolha3: "Casa Hermosa",
+    escolha4: " Palácio Dom Diego",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual é o primeiro número maior que 2007 cuja soma dos seus algarismos é igual à soma dos algarismos de 2007??",
+    escolha1: "2115",
+    escolha2: "2016",
+    escolha3: "2076",
+    escolha4: " 2070",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual dos seguintes animais terrestres podem ser considerados bípedes, em zoologia?",
+    escolha1: "Homem",
+    escolha2: "Girafas",
+    escolha3: "Porcos",
+    escolha4: " Cão",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o coletivo de cães?",
+    escolha1: "Matilha",
+    escolha2: "Rebanho",
+    escolha3: "Cardume",
+    escolha4: " Manada",
+    resposta: 1
+  },
+  {
+    pergunta: "A personagem Charlot foi criado por...",
+    escolha1: "Carlos Gardel",
+    escolha2: "Charlotte Brontë",
+    escolha3: "Charlie Chaplin",
+    escolha4: " Charles Aznavour",
+    resposta: 3
+  },
+  {
+    pergunta: "Quem foi provável assassino de John Kennedy?",
+    escolha1: "Eliott Ness",
+    escolha2: "ALexander Burr",
+    escolha3: "Lee Harvey Oswald",
+    escolha4: " Billy The Kid",
+    resposta: 3
+  },
+  {
+    pergunta: "Qual é o coletivo de cães?",
+    escolha1: "Matilha",
+    escolha2: "Rebanho",
+    escolha3: "Cardume",
+    escolha4: " Manada",
+    resposta: 1
+  },
+  {
+    pergunta: "A obra literária 'OS Sertões' de Euclides da Cunha está dividida em três partes. Quais são elas?",
+    escolha1: "A terra; O homem; A luta",
+    escolha2: "A terra; A luta; A guerra",
+    escolha3: "A luta; A terra; O sol",
+    escolha4: " A terra; A seca; A lua",
+    resposta: 1
+  },
+  {
+    pergunta: "Foi construído entre 1859 e 1869. De qual canal se trata?",
+    escolha1: "Panamá",
+    escolha2: "Suez",
+    escolha3: "Mancha",
+    escolha4: " Gibraltar",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual é o coletivo de cães?",
+    escolha1: "Matilha",
+    escolha2: "Rebanho",
+    escolha3: "Cardume",
+    escolha4: " Manada",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem é o autor de '1984'?",
+    escolha1: "George Orwell",
+    escolha2: "Aldous Huxley",
+    escolha3: "Ray Bradbury",
+    escolha4: "Philip K. Dick",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem pintou 'A Noite Estrelada'?",
+    escolha1: "Vincent van Gogh",
+    escolha2: "Leonardo da Vinci",
+    escolha3: "Pablo Picasso",
+    escolha4: "Claude Monet",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o país com a maior área territorial do mundo?",
+    escolha1: "Rússia",
+    escolha2: "Canadá",
+    escolha3: "China",
+    escolha4: "Estados Unidos",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem foi o primeiro homem a pisar na lua?",
+    escolha1: "Neil Armstrong",
+    escolha2: "Buzz Aldrin",
+    escolha3: "Michael Collins",
+    escolha4: "Yuri Gagarin",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o maior deserto do mundo?",
+    escolha1: "Antártica",
+    escolha2: "Saaara",
+    escolha3: "Deserto do Saara",
+    escolha4: "Gobi",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é a capital da Austrália?",
+    escolha1: "Camberra",
+    escolha2: "Sydney",
+    escolha3: "Melbourne",
+    escolha4: "Brisbane",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o maior oceano do mundo?",
+    escolha1: "Pacífico",
+    escolha2: "Atlântico",
+    escolha3: "Índico",
+    escolha4: "Ártico",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o maior planeta do Sistema Solar?",
+    escolha1: "Júpiter",
+    escolha2: "Saturno",
+    escolha3: "Netuno",
+    escolha4: "Marte",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem é o autor do livro 'Dom Quixote'?",
+    escolha1: "Miguel de Cervantes",
+    escolha2: "William Shakespeare",
+    escolha3: "Jorge Luis Borges",
+    escolha4: "Gabriel García Márquez",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o maior órgão do corpo humano?",
+    escolha1: "Pele",
+    escolha2: "Fígado",
+    escolha3: "Coração",
+    escolha4: "Pulmões",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem foi o primeiro presidente dos Estados Unidos?",
+    escolha1: "George Washington",
+    escolha2: "Thomas Jefferson",
+    escolha3: "Abraham Lincoln",
+    escolha4: "John Adams",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o maior animal terrestre?",
+    escolha1: "Elefante africano",
+    escolha2: "Girafa",
+    escolha3: "Hipopótamo",
+    escolha4: "Rinoceronte",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem é o criador da teoria da relatividade?",
+    escolha1: "Isaac Newton",
+    escolha2: "Nikola Tesla",
+    escolha3: "Albert Einstein",
+    escolha4: "Stephen Hawking",
+    resposta: 4
+  },
+  {
+    pergunta: "Qual é o elemento químico mais abundante no universo?",
+    escolha1: "Oxigênio",
+    escolha2: "Hidrogênio",
+    escolha3: "Carbono",
+    escolha4: "Nitrogênio",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual é o maior osso do corpo humano?",
+    escolha1: "Fêmur",
+    escolha2: "Tíbia",
+    escolha3: "Fíbula",
+    escolha4: "Crânio",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o ácido encontrado no limão?",
+    escolha1: "Ácido cítrico",
+    escolha2: "Ácido acético",
+    escolha3: "Ácido fórmico",
+    escolha4: "Ácido sulfúrico",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem é o autor de 'O Retrato de Dorian Gray'?",
+    escolha1: "Oscar Wilde",
+    escolha2: "Edgar Allan Poe",
+    escolha3: "Herman Melville",
+    escolha4: "Mark Twain",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o período de rotação da Terra?",
+    escolha1: "24 horas",
+    escolha2: "12 horas",
+    escolha3: "36 horas",
+    escolha4: "48 horas",
+    resposta: 1
+  },
+  {
+    pergunta: "Quem é o criador da série de televisão 'Os Simpsons'?",
+    escolha1: "Matt Groening",
+    escolha2: "Seth MacFarlane",
+    escolha3: "Trey Parker",
+    escolha4: "Mike Judge",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o nome do filme que ganhou o Oscar de Melhor Filme em 1994?",
+    escolha1: "Forrest Gump",
+    escolha2: "O Poderoso Chefão",
+    escolha3: "Titanic",
+    escolha4: "A Lista de Schindler",
+    resposta: 4
+  },
+  {
+    pergunta: "Quem é o diretor do filme 'Pulp Fiction'?",
+    escolha1: "Quentin Tarantino",
+    escolha2: "Martin Scorsese",
+    escolha3: "Steven Spielberg",
+    escolha4: "Christopher Nolan",
+    resposta: 1
+  },
+  {
+    pergunta: "Qual é o nome da série de TV baseada nos livros de George R. R. Martin?",
+    escolha1: "Breaking Bad",
+    escolha2: "Stranger Things",
+    escolha3: "Game of Thrones",
+    escolha4: "The Walking Dead",
+    resposta: 3
+  },
+  {
+    pergunta: "Qual é o nome da banda de rock liderada por Mick Jagger?",
+    escolha1: "The Beatles",
+    escolha2: "Led Zeppelin",
+    escolha3: "The Rolling Stones",
+    escolha4: "Queen",
+    resposta: 3
+  },
+  {
+    pergunta: "Qual é o álbum mais vendido de todos os tempos?",
+    escolha1: "Thriller - Michael Jackson",
+    escolha2: "Dark Side of the Moon - Pink Floyd",
+    escolha3: "Back in Black - AC/DC",
+    escolha4: "The Wall - Pink Floyd",
+    resposta: 1
+  },
+  {
+    pergunta: "Se um retângulo tem comprimento 10 cm e largura 4 cm, qual é a sua área?",
+    escolha1: "36 cm²",
+    escolha2: "40 cm²",
+    escolha3: "44 cm²",
+    escolha4: "48 cm²",
+    resposta: 2
+  },
+  {
+    pergunta: "Qual é o resultado da divisão de 144 por 12?",
+    escolha1: "11",
+    escolha2: "12",
+    escolha3: "13",
+    escolha4: "14",
+    resposta: 2
+  },
+  {
+    pergunta: "Se um círculo tem raio de 6 cm, qual é a sua circunferência?",
+    escolha1: "12π cm",
+    escolha2: "18π cm",
+    escolha3: "24π cm",
+    escolha4: "36π cm",
+    resposta: 3
+  },
+  {
+    pergunta: "Se um triângulo equilátero tem um lado de 6 cm, qual é a sua área?",
+    escolha1: "9√3 cm²",
+    escolha2: "12√3 cm²",
+    escolha3: "15√3 cm²",
+    escolha4: "18√3 cm²",
+    resposta: 1
+    }
+
+];
